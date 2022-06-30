550fc4988bbff5fc04a90bf00e436b1e638cef69
pyrareae <pyrareae@gmail.com>
2022-06-30 14:43:46 -0700

    test user sessions controller

--- notte-rails/test/controllers/user_sessions_controller_test.rb
+++ notte-rails/test/controllers/user_sessions_controller_test.rb
@@ -2,45 +2,21 @@ require "test_helper"
 
 class UserSessionsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @user_session = user_sessions(:one)
-  end
-
-  test "should get index" do
-    get log_in_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_log_in_url
-    assert_response :success
+    @user = users(:one)
   end
 
   test "should create user_session" do
     assert_difference("UserSession.count") do
-      post log_in_url, params: { user_session: { password: @user_session.password, username: @user_session.username } }
+      post log_in_url, params: { user_session: { password: 'password', username: @user.username } }
     end
 
-    assert_redirected_to log_in_url(UserSession.last)
-  end
-
-  test "should show user_session" do
-    get log_in_url(@user_session)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_log_in_url(@user_session)
-    assert_response :success
-  end
-
-  test "should update user_session" do
-    patch log_in_url(@user_session), params: { user_session: { password: @user_session.password, username: @user_session.username } }
-    assert_redirected_to log_in_url(@user_session)
+    assert_redirected_to root_url
   end
 
   test "should destroy user_session" do
+    post log_in_url, params: { user_session: { password: 'password', username: @user.username } }
     assert_difference("UserSession.count", -1) do
-      delete log_in_url(@user_session)
+      delete log_out_url
     end
 
     assert_redirected_to log_in_url
