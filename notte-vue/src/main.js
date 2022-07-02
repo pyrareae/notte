916597210043e07f5b6f44cc50e2a02177c2dd39
pyrareae <pyrareae@gmail.com>
2022-07-02 13:52:14 -0700

    persistant auth token

--- notte-vue/src/main.js
+++ notte-vue/src/main.js
@@ -10,9 +10,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+import { useSessionStore } from './stores/session';
+useSessionStore().checkForStored()
--- notte-vue/src/stores/session.js
+++ notte-vue/src/stores/session.js
@@ -14,6 +14,12 @@ export const useSessionStore = defineStore('session', {
     applyHeader() {
       ax.defaults.headers.common["authorization"] = `${this.token}`
     },
+    checkForStored() {
+      const restored = localStorage.getItem('auth')
+      if (!restored) return
+
+      this.token = restored
+    },
     logIn(username, password) {
       return new Promise((resolve, reject) => {
         if (this.isLoggedIn) return resolve(false)
@@ -22,6 +28,7 @@ export const useSessionStore = defineStore('session', {
           .then((resp) => {
             this.token = resp.data.token;
             this.applyHeader();
+            localStorage.setItem('auth', this.token)
             resolve(true);
           })
           .catch(reject);
@@ -31,6 +38,7 @@ export const useSessionStore = defineStore('session', {
     logOut() {
       this.token = null
       this.applyHeader();
+      localStorage.removeItem('auth')
     } 
   }
 })
